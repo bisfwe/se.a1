6897278ee87f3d57a5b916083fe0f568dfe76ff6
konradan <konradan@student.ethz.ch>
2017-09-27 16:31:26 +0200

    Location in Snack Bar, Requesting permissions everytime

--- SmartEnergy/app/src/main/java/ch/ethz/inf/se/a1/smartenergy/MainActivity.java
+++ SmartEnergy/app/src/main/java/ch/ethz/inf/se/a1/smartenergy/MainActivity.java
@@ -1,12 +1,19 @@
 package ch.ethz.inf.se.a1.smartenergy;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,14 +22,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStates;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -34,9 +44,19 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mCurrentLocation;
+    private LocationSettingsStates mLocationSettingsStates;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //check location permissions right at the start
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,7 +65,17 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "This is a snack bar", Snackbar.LENGTH_LONG)
+
+                updateLastLocation();
+
+                double latitude = 0.0;
+                double longitude = 0.0;
+                if (mCurrentLocation != null) {
+                    latitude = mCurrentLocation.getLatitude();
+                    longitude = mCurrentLocation.getLongitude();
+                }
+
+                Snackbar.make(view, "Last location: " + latitude + " " + longitude, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -58,6 +88,55 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        createLocationRequest();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ) {//Can add more as per requirement
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123);
+        }
+    }
+
+    private void updateLastLocation() {
+
+        checkPermission();
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Context context = getApplicationContext();
+                            CharSequence text = "Location updated!";
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                            mCurrentLocation = location;
+                        }
+                    }
+                });
+        return;
+
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //checkPermission();
+        createLocationRequest();
     }
 
     @Override
@@ -100,7 +179,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-            createLocationRequest();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -141,6 +219,8 @@ public class MainActivity extends AppCompatActivity
                 // All location settings are satisfied. The client can initialize
                 // location requests here.
                 // ...
+                mLocationSettingsStates = locationSettingsResponse.getLocationSettingsStates();
+
             }
         });
 
