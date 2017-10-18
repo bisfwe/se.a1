0b2435923c616a69d1136e26a28658ad68e81212
Simon Bienz <simon.bienz@triboni.com>
2017-10-18 22:05:57 +0200

    [WIP] check location permission on runtime

--- SmartEnergy/app/src/main/java/ch/ethz/inf/se/a1/smartenergy/GraphActivity.java
+++ SmartEnergy/app/src/main/java/ch/ethz/inf/se/a1/smartenergy/GraphActivity.java
@@ -2,28 +2,59 @@ package ch.ethz.inf.se.a1.smartenergy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class GraphActivity extends AppCompatActivity {
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, -1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
-        series.setSpacing(20);
+
+        mListView = (ListView) findViewById(R.id.stats_list_view);
+        final ArrayList<Day> dayList = new ArrayList<Day>();
+
+
+        String[] listItems = new String[7];
+        Calendar today = Calendar.getInstance();
+        today.setTime(new Date());
+        for (int i = 0; i < 7; i++){
+            today.add(Calendar.DATE, -1);
+            listItems[i] = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(today.getTime());
+        }
+
+//        for(int i = 0; i < dayList.size(); i++){
+//            Day day = dayList.get(i);
+//            listItems[i] = day.date.toString();
+//        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
+
+//        GraphView graph = (GraphView) findViewById(R.id.graph);
+//        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+//                new DataPoint(0, -1),
+//                new DataPoint(1, 5),
+//                new DataPoint(2, 3),
+//                new DataPoint(3, 2),
+//                new DataPoint(4, 6)
+//        });
+//        graph.addSeries(series);
+//        series.setSpacing(20);
 
     }
 }
